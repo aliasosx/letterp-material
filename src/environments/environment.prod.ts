70340bdfa4648056567db7ceeca6599ed97e47c0
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2018-11-10 22:35:59 +0700

    environment prod update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://35.240.213.37:5000/api/v1/',
+  imageUrl: 'http://35.240.213.37:5000/images/uploads/foods/',
+  token: 'eyJhbGciOiJSUzI1NiJ9.YWRtaW58c2F5eWFsaW5oQGdtYWlsLmNvbQ.KukFudI0XS2hNBzeuXVpb0VQ1s30J8hd9d6aKhCnToOKEl-_QDICBlp1DNnGlcrddPmFkF5hDd_PkWcNmhB_og'
+
 };
