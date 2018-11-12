2e96e5ea502050f99aa44309e26666a9220a4f25
alisaosx <aliasosx@hotmail.com>
2018-11-12 16:34:25 +0700

    add pos interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ const routes: Routes = [
   { path: 'food', component: FoodComponent },
   { path: 'terminal', component: TerminalComponent },
   { path: 'administrator', component: AdministratorComponent },
+  { path: 'pos', component: PosComponent },
 ];
 @NgModule({
   declarations: [
--- src/app/pages/pos/pos.component.ts
+++ src/app/pages/pos/pos.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-pos',
   templateUrl: './pos.component.html',
-  styleUrls: ['./pos.component.css']
+  styleUrls: ['./pos.component.scss']
 })
 export class PosComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private dataService: DataService) { }
+  food_types: any;
   ngOnInit() {
+    this.getFoodtype();
+  }
+  getFoodtype() {
+    this.dataService.getFoodTypes().subscribe(food_type => {
+      this.food_types = food_type;
+      console.log(food_type);
+    });
   }
 
 }
