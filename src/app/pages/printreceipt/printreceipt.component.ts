59b6a07fb78a09f79506980ba478575dbb7ca61c
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2018-11-24 00:52:51 +0700

    update router

--- src/app/pages/printreceipt/printreceipt.component.ts
+++ src/app/pages/printreceipt/printreceipt.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Route } from '@angular/router';
 
 @Component({
   selector: 'app-printreceipt',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrintreceiptComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    console.log(this.router.url);
   }
   print() {
     const printContent = document.getElementById("invoice-POS");
