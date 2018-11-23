6840d3f6fc914accb2bccbffe03c935618f1314e
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2018-11-24 01:45:29 +0700

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  url: 'http://35.240.213.37:5000/api/v1/',
-  imageUrl: 'http://35.240.213.37:5000/images/uploads/foods/',
+  url: 'http://localhost:5000/api/v1/',
+  imageUrl: 'http://localhost:5000/images/uploads/foods/',
   token: 'eyJhbGciOiJSUzI1NiJ9.YWRtaW58c2F5eWFsaW5oQGdtYWlsLmNvbQ.KukFudI0XS2hNBzeuXVpb0VQ1s30J8hd9d6aKhCnToOKEl-_QDICBlp1DNnGlcrddPmFkF5hDd_PkWcNmhB_og'
 
 };
