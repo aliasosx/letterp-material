3a993ad2435f8b4d5bcdc92b1a8e73de92a8c266
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2018-11-29 00:25:34 +0700

    update environment prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  url: 'http://localhost:5000/api/v1/',
-  imageUrl: 'http://localhost:5000/images/uploads/foods/',
-  userImageUrl: 'http://localhost:5000/images/uploads/users/',
+  url: 'http://letterp-api.local:5000/api/v1/',
+  imageUrl: 'http://letterp-api.local:5000/images/uploads/foods/',
+  userImageUrl: 'http://letterp-api.local:5000/images/uploads/users/',
   token: 'eyJhbGciOiJSUzI1NiJ9.YWRtaW58c2F5eWFsaW5oQGdtYWlsLmNvbQ.KukFudI0XS2hNBzeuXVpb0VQ1s30J8hd9d6aKhCnToOKEl-_QDICBlp1DNnGlcrddPmFkF5hDd_PkWcNmhB_og'
 
 };
