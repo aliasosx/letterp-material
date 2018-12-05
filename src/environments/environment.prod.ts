b89ac0bc39e36d25fb5b454af4dbaca65be63a8e
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2018-12-05 23:04:55 +0700

    add ssl backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  url: 'http://letterp-api.local:5000/api/v1/',
-  imageUrl: 'http://letterp-api.local:5000/images/uploads/foods/',
-  userImageUrl: 'http://letterp-api.local:5000/images/uploads/users/',
+  url: 'https://35.240.213.37:5000/api/v1/',
+  imageUrl: 'https://35.240.213.37:5000/images/uploads/foods/',
+  userImageUrl: 'https://35.240.213.37:5000/images/uploads/users/',
   token: 'eyJhbGciOiJSUzI1NiJ9.YWRtaW58c2F5eWFsaW5oQGdtYWlsLmNvbQ.KukFudI0XS2hNBzeuXVpb0VQ1s30J8hd9d6aKhCnToOKEl-_QDICBlp1DNnGlcrddPmFkF5hDd_PkWcNmhB_og'
 
 };
