d2f992a8ad24ed994bf7c3f4ef45e764964b3c2d
alisaosx <aliasosx@hotmail.com>
2018-12-14 23:32:21 +0700

    update new DOC host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  url: 'https://167.99.76.97:5000/api/v1/',
-  imageUrl: 'https://167.99.76.97:5000/images/uploads/foods/',
-  userImageUrl: 'https://167.99.76.97:5000/images/uploads/users/',
+  url: 'https://139.59.216.238:5000/api/v1/',
+  imageUrl: 'https://139.59.216.238:5000/images/uploads/foods/',
+  userImageUrl: 'https://139.59.216.238:5000/images/uploads/users/',
   token: 'eyJhbGciOiJSUzI1NiJ9.YWRtaW58c2F5eWFsaW5oQGdtYWlsLmNvbQ.KukFudI0XS2hNBzeuXVpb0VQ1s30J8hd9d6aKhCnToOKEl-_QDICBlp1DNnGlcrddPmFkF5hDd_PkWcNmhB_og'
 
 };
